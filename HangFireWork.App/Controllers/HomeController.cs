b4d71959a9d738009252aee1f102ecd65fd06480
buraksensoz <buraksensoz@gmail.com>
2022-05-18 19:36:24 +0300

    Add project files.

---
+++ HangFireWork.App/Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using HangFireWork.App.Models;
+using HangFireWork.App.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HangFireWork.App.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        public IActionResult AddSingleWork()
+        {
+            HangfireService.AddOnceWork();
+            return RedirectToAction("index");
+        }
+
+        public IActionResult AddScheduleWork()
+        {
+            HangfireService.AddScheduleWork();
+            return RedirectToAction("index");
+        }
+
+        public IActionResult AddRecurringWork()
+        {
+            HangfireService.AddRecurringWork();
+            return RedirectToAction("index");
+        }
+
+        public IActionResult ClearLogs()
+        {
+            HangfireService.ClearLogs();
+            return RedirectToAction("index");
+        }
+
+        public async Task<String> GetLogs()
+        {
+            var Logs= await HangfireService.ReadLogFileAsync();
+            var TableHtml = $"<table class='table table-striped'>{Logs}</table>";
+            return TableHtml;
+
+
+        }
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ HangFireWork.App/Services/HangfireService.cs
@@ -0,0 +1,78 @@
+﻿using Hangfire;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace HangFireWork.App.Services
+{
+    public class HangfireService
+    {
+
+        public static void AddOnceWork()
+        {
+            Hangfire.BackgroundJob.Enqueue(()=> AddLog2File("a Single Work"));
+            //Hangfire.RecurringJob.AddOrUpdate("Mission İmposible", () => AddText2File(atxt), Cron.Minutely);
+
+        }
+
+        public static void AddScheduleWork()
+        {
+            Hangfire.BackgroundJob.Schedule(() => AddLog2File("Schedule Work Delay 10sec"),TimeSpan.FromSeconds(10));
+        }
+
+
+        public static void AddRecurringWork()
+        {
+            Hangfire.RecurringJob.AddOrUpdate("Recurring Work", () => AddLog2File("Recurring 1 Minute Work"), Cron.Minutely);
+
+        }
+
+
+
+        public static  void AddLog2File(string aTxt)
+        {
+            var file = Path.Combine(Directory.GetCurrentDirectory(), "WorkerNotes", "Notes.txt");
+            if (!File.Exists(file))
+            {
+                using (StreamWriter swr = File.CreateText(file))
+                {
+                    
+                };
+            }
+
+            StreamWriter sw = File.AppendText(file);
+            sw.WriteLine($"<tr><td><b>{DateTime.Now}</b></td><td>[{aTxt}]</td></tr>");
+            sw.Close();
+        }
+
+
+        public static async Task<string> ReadLogFileAsync()
+        {
+            var file = Path.Combine(Directory.GetCurrentDirectory(), "WorkerNotes", "Notes.txt");
+            if (!File.Exists(file))
+            {
+                return "<tr><td>No Log Record</td></tr>";
+            }
+
+            using (StreamReader sr = new StreamReader(file)){
+                return await sr.ReadToEndAsync();
+            }
+            
+
+
+        }
+        public static void  ClearLogs()
+        {
+            var file = Path.Combine(Directory.GetCurrentDirectory(), "WorkerNotes", "Notes.txt");
+            if (File.Exists(file))
+            {
+                File.Delete(file);
+            }
+
+            
+
+
+
+        }
+    }
+}
